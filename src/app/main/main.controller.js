76271fb7671d74e6dd78ea3d4e13c0ff0287e8ac
William Calderipe <wcalderipe@gmail.com>
2015-11-18 01:33:18 -0200

    Remove useless components

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,32 +2,11 @@ class MainController {
   constructor ($timeout, webDevTec, toastr) {
     'ngInject';
 
-    this.awesomeThings = [];
-    this.classAnimation = '';
-    this.creationDate = 1447816075432;
-    this.toastr = toastr;
-
-    this.activate($timeout, webDevTec);
+    this.activate();
   }
 
-  activate($timeout, webDevTec) {
-    this.getWebDevTec(webDevTec);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
-  }
+  activate() {
 
-  getWebDevTec(webDevTec) {
-    this.awesomeThings = webDevTec.getTec();
-
-    angular.forEach(this.awesomeThings, (awesomeThing) => {
-      awesomeThing.rank = Math.random();
-    });
-  }
-
-  showToastr() {
-    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    this.classAnimation = '';
   }
 }
 
